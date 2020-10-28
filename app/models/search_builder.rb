71714d52666f82d7b128306314328de18306ff80
Mike Kanning <mkanning@upenn.edu>
2020-10-28 11:33:34 -0400

    use *:* as q param for empty search

--- app/models/search_builder.rb
+++ app/models/search_builder.rb
@@ -3,11 +3,9 @@
 class SearchBuilder < Blacklight::SearchBuilder
   include Blacklight::Solr::SearchBuilderBehavior
 
-  ##
-  # @example Adding a new step to the processor chain
-  #   self.default_processor_chain += [:add_custom_data_to_query]
-  #
-  #   def add_custom_data_to_query(solr_parameters)
-  #     solr_parameters[:custom] = blacklight_params[:user_value]
-  #   end
+  self.default_processor_chain += [:return_all_for_empty_query]
+
+  def return_all_for_empty_query(solr_parameters)
+    solr_parameters[:q] = '*:*' if solr_parameters[:q].empty?
+  end
 end
